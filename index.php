150a2b07018731dfde78cb5f24c417016295d8c6
Linda <linddddnpt04@gmail.com>
2025-11-07 18:23:10 +0700

    Menambahkan file index.php di branch main

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
-echo "Ini file index.php di branch main";
+include 'koneksi.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah Data</a>
+    <hr>
+
+    <?php
+    $query = "SELECT * FROM mahasiswa";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        echo "<table border='1' cellpadding='10'>";
+        echo "<tr><th>ID</th><th>Nama</th><th>Prodi</th></tr>";
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>".$row['id']."</td>";
+            echo "<td>".$row['nama']."</td>";
+            echo "<td>".$row['prodi']."</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Belum ada data mahasiswa.";
+    }
+    ?>
+</body>
+</html>
+
